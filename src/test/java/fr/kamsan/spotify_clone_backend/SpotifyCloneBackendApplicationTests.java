9b74147e5817ff999b15e99ff0172ef476b5c45d
Kamsan-dev <rim3968@hotmail.fr>
2025-05-21 21:40:46 +0200

    Initial commit

---
+++ src/test/java/fr/kamsan/spotify_clone_backend/SpotifyCloneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.kamsan.spotify_clone_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyCloneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
